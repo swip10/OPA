d919ea977863992acf181a13dc4f63930d2d939e
loic <loic.chevalier.lc@gmail.com>
2023-03-02 21:00:53 +0100

    remove print to debug Benjamin

--- src/config.py
+++ src/config.py
@@ -3,6 +3,5 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read(Path(__file__).parents[1].absolute() / "config" / "config.ini")
-print(Path(__file__).parents[1].absolute() / "config" / "config.ini")
 BINANCE_API_KEY = config['API']['BINANCE_API_KEY']
 BINANCE_API_SECRET = config['API']['BINANCE_API_SECRET']
