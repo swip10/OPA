24783f3d9824c647fb4b00633af4a55ff11a939a
loic <loic.chevalier.lc@gmail.com>
2023-03-02 20:49:35 +0100

    add print to debug Benjamin

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import configparser
 
 config = configparser.ConfigParser()
-config.read(Path(__file__).parents[1] / "config" / "config.ini")
+config.read(Path(__file__).parents[1].absolute() / "config" / "config.ini")
+print(Path(__file__).parents[1].absolute() / "config" / "config.ini")
 BINANCE_API_KEY = config['API']['BINANCE_API_KEY']
 BINANCE_API_SECRET = config['API']['BINANCE_API_SECRET']
