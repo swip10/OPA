a540d00eaaeb16bc1a8a63fb14e5eccebd714eac
loic <loic.chevalier.lc@gmail.com>
2023-03-07 18:12:32 +0100

    update api_hist to output more data and plot them

--- src/data_api_hist.py
+++ src/data_api_hist.py
@@ -1,5 +1,6 @@
 from binance.client import Client
 import json
+from tqdm import tqdm
 import pandas as pd
 import config
 
@@ -13,20 +14,21 @@ tickers = client.get_all_tickers()
 
 # Initialise un dictionnaire pour stocker les données de chaque ticker
 ticker_data = {}
-
+loading_tickers = tqdm(tickers[0:10])
 # Récupère les données de l'API sur chacun des tickers
-for ticker in tickers[0:5]:
-    klines = client.get_historical_klines(ticker['symbol'], Client.KLINE_INTERVAL_1DAY, "10 days ago")
+for ticker in loading_tickers:
+    loading_tickers.set_description(f"loading symbol {ticker['symbol']}")
+    klines = client.get_historical_klines(ticker['symbol'], Client.KLINE_INTERVAL_8HOUR, "1000 days ago")
 
     # Transforme les données en DataFrame
     data = pd.DataFrame(klines, columns=["timestamp", "open", "high", "low", "close", "volume", "close_time", "quote_asset_volume", "number_of_trades", "taker_buy_base_asset_volume", "taker_buy_quote_asset_volume", "ignore"])
+    data.drop(columns="ignore", inplace=True)
 
     # Change le type de la colonne timestamp en datetime
     data["timestamp"] = pd.to_datetime(data["timestamp"], unit="ms").dt.strftime('%Y-%m-%d %H:%M:%S')
 
     # Stocke la DataFrame correspondante dans le dictionnaire
     ticker_data[ticker['symbol']] = data.to_dict(orient='records')
-    print("OK pour le ticker " + ticker['symbol'])
 
 # Exporte les données au format JSON
 with open('ticker_data_hist.json', 'w') as f:
--- src/plots/historical_prices.py
+++ src/plots/historical_prices.py
@@ -1,8 +1,24 @@
+import json
 import pandas as pd
 import plotly.express as px
 
+with open("../ticker_data_hist.json", "r") as json_file:
+    hist_data = json.load(json_file)
+
+list_df = []
+for key in hist_data:
+    sub_df = pd.DataFrame(hist_data[key])
+    sub_df["symbol"] = key
+    list_df.append(sub_df)
+df = pd.concat(list_df, ignore_index=True)
+col_float = [c for c in df if "timestamp" != c and "symbol" != c]
+df[col_float] = df[col_float].astype(float)
+
+fig = px.line(df, x="timestamp", y="close", color='symbol')
+fig.show()
 
 df = pd.read_csv("ticker_avg_price.csv", index_col=0)
+df = df.astype(float)
 
 df = df.melt(id_vars=["timestamp"],
              var_name="currency",
