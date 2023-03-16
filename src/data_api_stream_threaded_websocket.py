3791978b074ce620b9a9347fa22fe6806c5723f7
loic <loic.chevalier.lc@gmail.com>
2023-03-16 10:38:28 +0100

    add stream data into DB

--- src/data_api_stream_threaded_websocket.py
+++ src/data_api_stream_threaded_websocket.py
@@ -1,24 +1,44 @@
 from binance.streams import ThreadedWebsocketManager
+from binance.client import Client
+from datetime import datetime
 from time import sleep
+import config
+import test_sql
 
-symbol = 'BNBBTC'
+client = Client(config.BINANCE_API_KEY, config.BINANCE_API_SECRET)
+tickers = config.get_tickers(client)
 
 twm = ThreadedWebsocketManager()
 # start is required to initialise its internal loop
 twm.start()
 
 
-def handle_socket_message(msg):
+def transform_and_append_database(msg):
+    connector = sql_client.get_db_client()
     print(f"message type: {msg['e']}")
-    print(msg)
+    kline = msg["k"]
+    data = dict()
+    data["timestamp"] = datetime.fromtimestamp(kline["t"]/1000).strftime('%Y-%m-%d %H:%M:%S')
+    data["close_time"] = datetime.fromtimestamp(kline["T"]/1000).strftime('%Y-%m-%d %H:%M:%S')
+    data["open"] = kline["o"]
+    data["high"] = kline["h"]
+    data["low"] = kline["l"]
+    data["close"] = kline["c"]
+    data["volume"] = kline["v"]
+    data["quote_asset_volume"] = kline["q"]
+    data["number_of_trades"] = kline["n"]
+    data["taker_buy_base_asset_volume"] = kline["V"]
+    data["taker_buy_quote_asset_volume"] = kline["Q"]
+    print(data)
+    sql_client.add_line_to_database(data, kline["s"], connector, close=True)
 
 
 # example of function to call for opening a steam - todo define one handler for each function
-twm.start_depth_socket(callback=handle_socket_message, symbol=symbol)
+# twm.start_depth_socket(callback=handle_socket_message, symbol=symbol)
+for ticker in tickers:
+    twm.start_kline_socket(callback=transform_and_append_database, symbol=ticker, interval='1s')
 
-twm.start_kline_socket(callback=handle_socket_message, symbol=symbol)
-
-twm.start_aggtrade_socket(callback=handle_socket_message, symbol=symbol)
+# twm.start_aggtrade_socket(callback=handle_socket_message, symbol=symbol)
 
 sleep(5)
 twm.stop()
--- src/test_sql.py
+++ src/test_sql.py
@@ -1,56 +1,27 @@
 import sqlite3
 import json
+import sql_client
+
 
 # Ouverture du fichier JSON
-with open('ticker_data_hist.json') as f:
+with open('../ticker_data_hist.json') as f:
     data = json.load(f)
 
 # Récupération des tickers présent dans le JSON
 keys = data.keys()
 
 # Connexion à la base de données
-conn = sqlite3.connect('BDD_hist.sqlite')
-
+conn = sql_client.get_db_client()
 c = conn.cursor()
 
 # Création de la boucle pour créer et alimenter les tables
 for key in keys:
-    c.execute(f'''CREATE TABLE IF NOT EXISTS {key}
-                (symbol TEXT,
-                 timestamp TEXT, 
-                 open FLOAT, 
-                 high FLOAT, 
-                 low FLOAT, 
-                 close FLOAT, 
-                 volume FLOAT, 
-                 close_time TEXT, 
-                 quote_asset_volume FLOAT, 
-                 number_of_trades INTEGER, 
-                 taker_buy_base_asset_volume FLOAT, 
-                 taker_buy_quote_asset_volume FLOAT, 
-                 PRIMARY KEY (timestamp, symbol))''')
+    sql_client.create_table_database(c, key)
 
     # Insertion des données dans la table
-
     for d in data[key]:
-        symbol = key
-        timestamp = d['timestamp']
-        open_price = d['open']
-        high = d['high']
-        low = d['low']
-        close = d['close']
-        volume = d['volume']
-        close_time = d['close_time']
-        quote_asset_volume = d['quote_asset_volume']
-        number_of_trades = d['number_of_trades']
-        taker_buy_base_asset_volume = d['taker_buy_base_asset_volume']
-        taker_buy_quote_asset_volume = d['taker_buy_quote_asset_volume']
-
-        c.execute(f"INSERT INTO {key} VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                (symbol, timestamp, open_price, high, low, close, volume, close_time, quote_asset_volume, number_of_trades, taker_buy_base_asset_volume, taker_buy_quote_asset_volume))
-
+        print(key)
+        sql_client.add_line_to_database(d, key, conn, close=False)
 
 # Validation des changements et fermeture de la connexion à la base de données
-conn.commit()
 conn.close()
-
