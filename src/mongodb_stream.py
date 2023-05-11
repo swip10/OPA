43ab4eedee8a253aaa147eff9aeb86823d5c6a35
loic <loic.chevalier.lc@gmail.com>
2023-05-11 22:00:21 +0200

    simplify stream data mongodb

--- src/mongodb_stream.py
+++ src/mongodb_stream.py
@@ -1,6 +1,5 @@
 from binance.streams import ThreadedWebsocketManager
 from binance.client import Client
-from datetime import datetime
 from time import sleep
 from config import config
 from src.db.mongodb import MongoOPA, Collection
@@ -18,28 +17,9 @@ client = MongoOPA(
 )
 client.create_collections(reset=True)
 
-
-def transform_and_append_database(msg):
-    kline = msg["k"]
-    data = dict()
-    data["symbol"] = kline["s"]
-    data["timestamp"] = datetime.fromtimestamp(kline["t"]/1000).strftime('%Y-%m-%d %H:%M:%S')
-    data["close_time"] = datetime.fromtimestamp(kline["T"]/1000).strftime('%Y-%m-%d %H:%M:%S')
-    data["open"] = kline["o"]
-    data["high"] = kline["h"]
-    data["low"] = kline["l"]
-    data["close"] = kline["c"]
-    data["volume"] = kline["v"]
-    data["quote_asset_volume"] = kline["q"]
-    data["number_of_trades"] = kline["n"]
-    data["taker_buy_base_asset_volume"] = kline["V"]
-    data["taker_buy_quote_asset_volume"] = kline["Q"]
-    client.insert_document_to_collection(doc=data, collection_name=Collection.KLINES)
-
-
 # twm.start_depth_socket(callback=handle_socket_message, symbol=symbol)
 for ticker in tickers:
-    twm.start_kline_socket(callback=transform_and_append_database, symbol=ticker, interval='1s')
+    twm.start_kline_socket(callback=client.callback_stream_msg, symbol=ticker, interval='1s')
 
 # twm.start_aggtrade_socket(callback=handle_socket_message, symbol=symbol)
 
