41708d4065420f452b3a0a12665794098ac36eec
Bervurey <125876929+Bervurey@users.noreply.github.com>
2023-10-10 13:15:41 +0200

    Update stream

--- src/postgreSQL_stream.py
+++ src/postgreSQL_stream.py
@@ -7,6 +7,7 @@ from src.db.postgres import Postgres
 
 client = Client(config.BINANCE_API_KEY, config.BINANCE_API_SECRET)
 tickers = config.get_tickers(client)
+print(tickers)
 
 twm = ThreadedWebsocketManager()
 # start is required to initialise its internal loop
@@ -17,7 +18,7 @@ postgres_client = Postgres()
 postgres_client.create_tables(tickers, reset=True)
 
 for ticker in tickers:
-    twm.start_kline_socket(callback=postgres_client.callback_stream_msg, symbol=ticker, interval='1s')
+    twm.start_kline_socket(callback=lambda msg: postgres_client.callback_stream_msg(msg, ticker), symbol=ticker, interval='1s')
  
 sleep(5)
 
